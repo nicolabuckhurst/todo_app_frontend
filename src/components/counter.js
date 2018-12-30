279c3b91927981993d548207bf3a7cff68542b23
Nicola Buckhurst <nicolabuckhurst@gmail.com>
2018-12-30 21:08:58 +0000

    added a component for counter

---
+++ src/components/counter.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+//pass a prop called count to this component containing the total number of tasks remaining
+class Counter extends React.Component{
+   render(){
+       return(
+            <div className="row mb-4 justify-content-center">
+                <div className="col-10 text-center">
+                    <button type="button" className="btn btn-lg" style={styles["counter-button"]}>
+                        Tasks Remaining: <span className="badge badge-light ml-2">{this.props.count}</span>
+                        <span className="sr-only">tasks to do</span>
+                    </button>
+                </div>
+            </div> 
+       )
+   }
+}
+
+const styles={
+    "counter-button": {
+        "background-color": "midnightblue",
+        "color": "white"
+    }
+}
+
+export default Counter
