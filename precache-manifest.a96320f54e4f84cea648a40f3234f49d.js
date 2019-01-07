c79ae12051494636b8efc58672de3f158f2291d6
Nicola Buckhurst <nicolabuckhurst@gmail.com>
2019-01-07 20:20:12 +0000

    Updates

---
+++ precache-manifest.a96320f54e4f84cea648a40f3234f49d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3a94bf3e3dc0d020227",
+    "url": "/todo_app_frontend/static/css/main.cb8b175a.chunk.css"
+  },
+  {
+    "revision": "c3a94bf3e3dc0d020227",
+    "url": "/todo_app_frontend/static/js/main.c3a94bf3.chunk.js"
+  },
+  {
+    "revision": "acaa8cba34849edff304",
+    "url": "/todo_app_frontend/static/js/1.acaa8cba.chunk.js"
+  },
+  {
+    "revision": "c0acfcae6dbc01d4e16d",
+    "url": "/todo_app_frontend/static/js/runtime~main.c0acfcae.js"
+  },
+  {
+    "revision": "9bbbb4aee0e0217154e808f59880f8a1",
+    "url": "/todo_app_frontend/static/media/green-chameleon-21532-unsplash.9bbbb4ae.jpg"
+  },
+  {
+    "revision": "d126ce4380dd8d3a1d6ded7018feef17",
+    "url": "/todo_app_frontend/index.html"
+  }
+];
